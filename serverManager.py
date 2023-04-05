c0bfdf9cc30a7c4b25bd4dc6bf90e0287436e4ad
Yasson <devochtyasson1@gmail.com>
2023-04-05 13:24:40 +0200

    pingresults

--- serverManager.py
+++ serverManager.py
@@ -1,7 +1,6 @@
 import json
 import sys
-import platform
-import subprocess
+from ping3 import ping
 
 
 def add_server(naam, list):
@@ -20,27 +19,30 @@ def show_servers(list):
         print(item)
 
 
-def myping(host):
-    parameter = "-n" if platform.system().lower() == "windows" else '-c'
-    command = ['ping', parameter, "4", host]
-    response = subprocess.call(command)
-    if response == 0:
-        return True
+def myping3(host):
+    """check if server is online"""
+    if ping(host) is False:
+        return "Server offline"
     else:
-        return False
+        return "Server online"
 
 
 def main():
-    # management modus
-    server_lijst = []
+    servernames_and_status = []
+    server_lijst = []  # maak een lijst met servers
+    # open json files
     with open("serverlijst.json", "r") as j:
         server_lijst = json.load(j)
+    with open("pingresults.json", "r") as p:
+        servernames_and_status = json.load(p)
 
+    # management modus
+    # terminal interactive interface
     if len(sys.argv) < 2:
         print("Welkom bij de server manager\n")
         print("Kies een van de drie opties\n\n")
         keuze = input("1. Server toevoegen\n2. Server verwijderen\n\
-3. Serverlijst tonen\n\n")
+3. Serverlijst tonen\n4. Server pingen\n\n")
 
         if (keuze == "1"):
             add_server(input("Welke server toevoegen?\n"), server_lijst)
@@ -48,8 +50,16 @@ def main():
             remove_server(input("Welke server verwijderen?\n"), server_lijst)
         elif (keuze == "3"):
             show_servers(server_lijst)
+        elif (keuze == "4"):
+            server_to_ping = input("Welke server wil je pingen?\n")
+            server_status = myping3(server_to_ping)
+            print(server_status)
+            servernames_and_status.append({server_to_ping: server_status})
+            with open("pingresults.json", "w") as p:
+                json.dump(servernames_and_status, p)
         else:
             print("Dit is geen geldige keuze")
+    # command-line arguments
     elif sys.argv[1] == "manage":
         if sys.argv[2] == "add":
             add_server(sys.argv[3], server_lijst)
@@ -58,15 +68,17 @@ def main():
         elif sys.argv[2] == "show":
             show_servers(server_lijst)
 
+    # check modus
+    elif sys.argv[1] == "check":
+        server_to_ping = sys.argv[2]
+        server_status = myping3(server_to_ping)
+        print(server_status)
+        servernames_and_status.append({server_to_ping: server_status})
+        with open("pingresults.json", "w") as p:
+            json.dump(servernames_and_status, p)
+
     with open("serverlijst.json", "w") as j:
         json.dump(server_lijst, j)
-
-    # check modus
-    if sys.argv[1] == "check":
-        result = print(myping(sys.argv[2]))
-
-        with open("pingresults.json", "w") as p:
-            json.dump(result, p)
 
 
 if __name__ == "__main__":
